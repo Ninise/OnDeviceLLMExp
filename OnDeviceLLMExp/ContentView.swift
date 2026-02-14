a726c1fe952c30dcc613a0b549d9f5632ca9ae78
Ninise <nikitin.ninise@gmail.com>
2026-02-14 04:45:29 -0800

    Initial Commit

---
+++ OnDeviceLLMExp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnDeviceLLMExp
+//
+//  Created by Nikita Nikitin on 2026-02-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnDeviceLLMExp/OnDeviceLLMExpApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnDeviceLLMExpApp.swift
+//  OnDeviceLLMExp
+//
+//  Created by Nikita Nikitin on 2026-02-14.
+//
+
+import SwiftUI
+
+@main
+struct OnDeviceLLMExpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
